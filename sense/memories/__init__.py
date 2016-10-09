2f174d9bde5dd3dfb8510e120c6e677e17635682
Diego Rubin <rubin.diego@gmail.com>
2016-10-09 18:55:56 -0300

    functions to find and insert in tables.

--- sense/memories/__init__.py
+++ sense/memories/__init__.py
@@ -5,17 +5,24 @@ from sense.settings import TINY_DB_PATH
 def db():
     return TinyDB(TINY_DB_PATH)
 
+def find_in_table(table, name):
+    query = Query()
+    return table.search(query.name == name)
+
+def save_in_table(table, name, value):
+    attributes = {'name': name, 'value': value}
+    query = Query()
+    memory = table.search(query.name == name)
+    if len(memory) == 0:
+        table.insert(attributes)
+    else:
+        table.update(attributes, query.name == name)
+
 def memories():
     return db().table('memories')
 
 def remember(name, value):
-    attributes = {'name': name, 'value': value}
-    query = Query()
-    memory = memories().search(query.name == name)
-    if len(memory) == 0:
-        memories().insert(attributes)
-    else:
-        memories().update(attributes, query.name == name)
+    save_in_table(memories(), name, value)
 
 def remind(name):
     query = Query()
